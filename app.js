e8031463599f612b8cf602f5487ebde3b564b4ad
etzidanpea <jodhykotambunan@gmail.com>
2022-06-03 17:11:01 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+(function () {
+  [...document.querySelectorAll(".control")].forEach((button) => {
+    button.addEventListener("click", function () {
+      document.querySelector(".active-btn").classList.remove("active-btn");
+      this.classList.add("active-btn");
+      document.querySelector(".active").classList.remove("active");
+      document.getElementById(button.dataset.id).classList.add("active");
+    });
+  });
+  document.querySelector(".theme-btn").addEventListener("click", () => {
+    document.body.classList.toggle("light-mode");
+  });
+})();
+
+// contact
+const scriptURL = "https://script.google.com/macros/s/AKfycbzmth1NISl2OpZvxC_AtgFUbvrB3uyaxMRBBwL1KpbjWRKaQc0TXA1-l2jFCqwTRStEMA/exec";
+const form = document.forms["zidane-contact-form"];
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, {
+    method: "POST",
+    body: new FormData(form),
+  })
+    .then((response) => {
+      form.reset();
+      alert("Pesan Berhasil Terkirim");
+      console.log("Success!", response);
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
